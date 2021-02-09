0b640f0954d6a10c2bb830170ae4b58df29ca2dc
jian <masichyun77@hanmail.net>
2021-02-09 10:45:59 +0900

    매퍼수정

--- src/main/java/com/fourmeeting/bee/notice/model/vo/Notice.java
+++ src/main/java/com/fourmeeting/bee/notice/model/vo/Notice.java
@@ -10,7 +10,7 @@ public class Notice {
 	private String noticeCont;
 	private Timestamp noticeDate;
 	private char noticeDelYN;
-	
+	private String memberId;
 
 	public Notice() {
 		super();
@@ -18,7 +18,7 @@ public class Notice {
 	}
 
 	public Notice(int noticeNo, int memberNo, String noticeCategory, String noticeTitle, String noticeCont,
-			Timestamp noticeDate, char noticeDelYN) {
+			Timestamp noticeDate, char noticeDelYN, String memberId) {
 		super();
 		this.noticeNo = noticeNo;
 		this.memberNo = memberNo;
@@ -27,50 +27,73 @@ public class Notice {
 		this.noticeCont = noticeCont;
 		this.noticeDate = noticeDate;
 		this.noticeDelYN = noticeDelYN;
+		this.memberId = memberId;
 	}
-	
+
 	public int getNoticeNo() {
 		return noticeNo;
 	}
+
 	public void setNoticeNo(int noticeNo) {
 		this.noticeNo = noticeNo;
 	}
+
 	public int getMemberNo() {
 		return memberNo;
 	}
+
 	public void setMemberNo(int memberNo) {
 		this.memberNo = memberNo;
 	}
+
 	public String getNoticeCategory() {
 		return noticeCategory;
 	}
+
 	public void setNoticeCategory(String noticeCategory) {
 		this.noticeCategory = noticeCategory;
 	}
+
 	public String getNoticeTitle() {
 		return noticeTitle;
 	}
+
 	public void setNoticeTitle(String noticeTitle) {
 		this.noticeTitle = noticeTitle;
 	}
+
 	public String getNoticeCont() {
 		return noticeCont;
 	}
+
 	public void setNoticeCont(String noticeCont) {
 		this.noticeCont = noticeCont;
 	}
+
 	public Timestamp getNoticeDate() {
 		return noticeDate;
 	}
+
 	public void setNoticeDate(Timestamp noticeDate) {
 		this.noticeDate = noticeDate;
 	}
+
 	public char getNoticeDelYN() {
 		return noticeDelYN;
 	}
+
 	public void setNoticeDelYN(char noticeDelYN) {
 		this.noticeDelYN = noticeDelYN;
 	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
 	
 	
 	
