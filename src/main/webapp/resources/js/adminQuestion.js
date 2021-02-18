a9468dd3dcc0a592b6b55230e0f8f1c27287b437
pj <pj920520@gmail.com>
2021-02-18 18:15:13 +0900

    adminQuestion.js

--- src/main/webapp/resources/js/adminQuestion.js
+++ src/main/webapp/resources/js/adminQuestion.js
@@ -210,7 +210,7 @@ $(function(){
 			//답변 삭제 (admin_answer_view_modal_submit)
 			$("#admin_answer_view_modal_submit").click(function() {
 				
-				var result = confirm("정말 삭제하시겠습니까? /// "+tableQuestionNo);
+				var result = confirm("정말 삭제하시겠습니까?");
 				if (result) {
 
 					$.ajax({
